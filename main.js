d40bfc4df653a929d6b587ece651a2e9399b1c7a
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-10-08 08:06:53 -0300

    ghfhfh

--- main.js
+++ main.js
@@ -1,34 +1,19 @@
 function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container');
-    let cartao = document.createElement('div');
-    cartao.className = 'flashcard';
-  
-    let front = document.createElement('div');
-    front.className = 'front';
-    front.innerHTML = `
-      <h3>${categoria}</h3>
-      <div class="cartao-pergunta question">
-        <p>${pergunta}</p>
-      </div>
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+
+    cartao.innerHTML = `
+    <div class="flashcard" onclick="this.classList.toggle('flip')">
+        <div class="cartao-pergunta question">
+            <h3>${categoria}</h3>
+            <p>${pergunta}</p>
+        </div>
+        <div class="cartao-resposta answer">
+            <p>${resposta}</p>
+        </div>
+    </div>
     `;
-  
-    let back = document.createElement('div');
-    back.className = 'back';
-    back.innerHTML = `
-      <div class="cartao-resposta answer">
-        <p>${resposta}</p>
-      </div>
-    `;
-  
-    cartao.appendChild(front);
-    cartao.appendChild(back);
-  
+    
     container.appendChild(cartao);
-  
-    cartao.addEventListener('click', () => {
-      cartao.classList.toggle('flipped');
-    });
-  }
-  cartao.addEventListener('click', () => {
-    cartao.classList.toggle('flipped');
-});
+}
