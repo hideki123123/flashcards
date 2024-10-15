4c50fa76c9d8e1daeb42bca365c014a066901605
hideki123123 <105948160+hideki123123@users.noreply.github.com>
2024-10-15 08:29:52 -0300

    asdadasd

--- main.js
+++ main.js
@@ -20,3 +20,101 @@ function criaCartao(categoria, pergunta, resposta) {
 function showAnser(card){
 
 }
+criaCartao (
+    'Química orgânica',
+    'Como a tetravalência do carbono influencia a diversidade dos compostos orgânicos?',
+    'Permitindo a formação de quatro ligações covalentes simultâneas, gerando uma enorme variedade de estruturas.')
+    
+    criaCartao(
+    'Química orgânica', 
+    'Explique a importância das diferentes formas alotrópicas do carbono.',
+    'As variações de estruturas e disposições do carbono podem gerar carbono, grafite e fulerenos.')
+    
+    criaCartao(
+    'Química orgânica',
+    'Qual a relevância da hibridação sp2 no comportamento de compostos aromáticos?',
+    'Permite a formação de ligações pi conjugadas em anéis aromáticos, como o benzeno, conferindo estabilidade e propriedades únicas aos compostos aromáticos.')
+    
+    criaCartao(
+    'Química orgânica',
+    'Descreva a hibridação sp3',
+    'Ocorre quando um átomo de carbono forma quatro ligações sigma, resultando na geometria tetraédrica. Essa formação é crucrial na formação  de compostos orgânicos complexos.')
+    
+    criaCartao(
+    'Química orgânica',
+    'O que são cadeias carbônicas?',
+    'Estruturas formadas por átomos de carbono interligados.')
+    criaCartao(
+    'Urbanizacao',
+    'qual a diferença entre município e cidade?',
+    'municipio é a entidade administrativa, enquanto  cidade é a área urbana que serve como sede do município')
+    
+    criaCartao(
+    'urbanizaçao',
+    'o que é um sítio urbano?',
+    'é a localização geografica e as características naturais de um lugar onde uma cidade se desenvolve, influenciando sua formação e crescimento')
+    
+    criaCartao(
+    'urbanização',
+    'quais sao as criticas ao processo de gentrificaçao',
+    'as criticas incluem aumento da desigualdade social, perda de identidade cultural e exclusao dos moradores originais')
+    
+    criaCartao(
+    'história',
+    'qual foi o impacto da revolução francesa nas primeiras rebeliões na América espanhola?',
+    'inspirou movimentos de independência e rebelioes cobrra o dominio colonial espanhol na america latina')
+    
+    criaCrtao(
+    'historia',
+    'como a revolução americana influenciou as prineiras rebelioes na america espanhola?',
+    'encorajou e inspirou a independência de uma colonia encorajando a lutar pela independência')
+    
+    criaCartao(
+    'historia',
+    'quem foi Tupac amaru ll e qual sua importância nas primeiras rebeliões?',
+    'foi uma referência no Peru em 1780 simbolizando a resistencia contra a opressao colonial')
+    
+    criaCartao(
+    'historia',
+    'qual foi umas das cUsas dss principais causas das primeiras revelioes na americ espanhola?',
+    'influência das ideias iluministas e exemplo das revolucoes contra os colonos')
+    
+    criaCartao(
+    'Sociologia',
+    'Qual a relação entre poder de dominação legítima segundo Max Weber?',
+    'Weber define dominação legítima como a aceitação do poder como válido e justo pelos subordinados.')
+    
+    criaCartao(
+    'Sociologia',
+    'Quais são as formas de poder',
+    'Coercitivo, que usa força e ameaças; poder econômico e poder ideológico')
+    
+    criaCartao(
+    'Sociologia',
+    'Como Max Weber define a dominação carismática?',
+    'Ela é baseada na devoção a um líder carismático.')
+    
+    criaCartao(
+    'Sociologia',
+    'Qual a diferença entre dominação legal-racional e dominação tradicional?',
+    'Legal-racional é baseada em normas e regras impessoais e formalizadas, enquanto a tradicional depende da continuidade histórica')
+    
+    criaCartao(
+    'Sociologia',
+    'O que Émile Durkheim entende por fato social?',
+    'Maneiras de agir, pensar e sentir exteriores e anteriores ao indivíduo')
+    
+    criaCartao(
+    'Sociologia',
+    'O que é a mais valia segundo Karl Marx?',
+    'É o valor extra produzido pelo trabalhador que é apropriado pelo capitalista.')
+    
+    criaCartao(
+    'Sociologia',
+    'Explique a teoria da acumulação primitiva de Karl Marx',
+    'É o processo histórico que separou os produtores diretos dos meios de produção, criando uma classe trabalhadora desprovida de bens e uma classe capitalista que controla os meios de produção.')
+    
+    criaCartao(
+    'Sociologia',
+    'Como Marx define o fetichismo da mercadoria?',
+    'Ocorre quando as relações sociais de produção se escondem por trás das relações entre meracadorias, dando a entender que objetos tem valor intrínseco,  independente do trabalho humano.')
